a3e13c99208e0ebb5c51819fbea936911880ffef
Tomas <fornarat@gmail.com>
2017-03-21 19:14:01 +0100

    fixing docker repo

--- docker-build.sh
+++ docker-build.sh
@@ -1,7 +1,7 @@
 sitehound_version="3.2.9"
-docker build --tag docker.hyperiongray.com/sitehound:$sitehound_version .
+docker build --tag hyperiongray/sitehound:$sitehound_version .
 
 # to set different properties
-# docker run -d -p 0.0.0.0:5081:5081 --name=sitehound-3.2.2 -v settings_prod.py:/root/sitehound/ui/settings.py --hostname=sitehound --link mongodb:mongodb --link kafkacontainer:hh-kafka --link elasticsearch:hh-elasticsearch docker.hyperiongray.com/sitehound:3.2.2
+# docker run -d -p 0.0.0.0:5081:5081 --name=sitehound-$sitehound_version -v settings_prod.py:/root/sitehound/ui/settings.py --hostname=sitehound --link mongodb:mongodb --link kafkacontainer:hh-kafka --link elasticsearch:hh-elasticsearch docker.hyperiongray.com/sitehound:$sitehound_version
 
-docker run -d -p 0.0.0.0:5081:5081 --name=sitehound-3.2.2 --hostname=sitehound --link mongodb:mongodb --link kafkacontainer:hh-kafka --link elasticsearch:hh-elasticsearch docker.hyperiongray.com/sitehound:$sitehound_version
+docker run -d -p 0.0.0.0:5081:5081 --name=sitehound-$sitehound_version --hostname=sitehound --link mongodb:mongodb --link kafkacontainer:hh-kafka --link elasticsearch:hh-elasticsearch docker.hyperiongray.com/sitehound:$sitehound_version
