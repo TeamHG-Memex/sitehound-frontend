4048e14e15f6c924d9cc844f52268e9e56fe32ec
Tomas <fornarat@gmail.com>
2017-08-14 10:56:21 +0200

    showing filtering with checkboxes with tabs

--- ui/static/js3/controller/new-deep-crawl.js
+++ ui/static/js3/controller/new-deep-crawl.js
@@ -121,6 +121,26 @@ function ($scope, $filter, seedFactory, fetchService, seedUrlFactory, trainingSe
 
 
 
+
+	/** tabs */
+	  var tabs = [
+          { title: 'Search Engines', content: "Tabs will become paginated if there isn't enough room for them."},
+          // { title: 'Two', content: "You can swipe left and right on a mobile device to change tabs."},
+          { title: 'Seeds', content: "You can bind the selected tab via the selected attribute on the md-tabs element."},
+          { title: 'Onions', content: "If you set the selected tab binding to -1, it will leave no tab selected."},
+          { title: 'DeepDeep', content: "If you remove a tab, it will try to select a new one."},
+          // { title: 'Six', content: "There's an ink bar that follows the selected tab, you can turn it off if you want."},
+          // { title: 'Seven', content: "If you set ng-disabled on a tab, it becomes unselectable. If the currently selected tab becomes disabled, it will try to select the next tab."},
+          // { title: 'Eight', content: "If you look at the source, you're using tabs to look at a demo for tabs. Recursion!"},
+          // { title: 'Nine', content: "If you set md-theme=\"green\" on the md-tabs element, you'll get green tabs."},
+          // { title: 'Ten', content: "If you're still reading this, you should just go check out the API docs for tabs!"}
+        ];
+        selected = null,
+        previous = null;
+    $scope.tabs = tabs;
+    $scope.selectedIndex = 2
+
+
 /** aggregated results by source */
     /*
     var resultStructOriginal = {
